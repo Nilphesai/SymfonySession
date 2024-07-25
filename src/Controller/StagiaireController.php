2790bef570017a2c13469ea54a4452cf4cf6f1f6
Gael <dt.gael.ho@gmail.com>
2024-07-25 16:59:15 +0200

    SymfonySession : new Stagiaire (en cours)

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Stagiaire;
+use App\Form\StagiaireType;
 use App\Repository\StagiaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,5 +28,32 @@ class StagiaireController extends AbstractController
         return $this->render('stagiaire/show.html.twig', [
             'stagiaire' => $stagiaire,
         ]);
+    }
+
+    #[Route('/stagiaire/new', name: 'new_stagiaire')]
+    public function new(Stagiaire $stagiaire = null, Request $request, EntityManagerInterface $entityManager):Response
+    {
+        if(!$stagiaire){
+            $stagiaire = new Stagiaire();
+        }
+        
+
+        $form = $this->createForm(StagiaireType::class, $stagiaire);
+        
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+
+            $stagiaire = $form->getData();
+            $entityManager->persist($stagiaire);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_stagiaire');
+        }
+
+        return $this->render('stagiaire/new.html.twig', [
+            'formAddStagiaire' => $form,
+            'edit' => $stagiaire->getId(),
+        ]);    
     }
 }
